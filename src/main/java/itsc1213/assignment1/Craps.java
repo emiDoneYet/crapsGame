fbd3f8c6b61895d7bd699fbb4bc6926b77192528
geeky <geeky@DESKTOP-52ARM9A>
2021-01-26 17:36:00 -0500

    Created template program for craps assignment.

---
+++ src/main/java/itsc1213/assignment1/Craps.java
@@ -0,0 +1,23 @@
+/* 
+ Emily Breneman
+ 01.26.2021
+ Assignment 1
+*/
+package itsc1213.assignment1;
+
+/*
+ Simulation of the Craps dice game
+ If the first roll of 2 die sums to a 7 or 11, the round is won. If the first
+ roll of 2 die sums to 2, 3, or 12 ("craps"), the round is lost. Tally the win 
+ or loss. 
+ If none of the above is rolled initially, the total rolled (sum) is saved as
+ the point (a point is established). Continue rolling the dice until either a 7
+ is rolled or the point is rolled (ending the round). If the point is rolled 
+ before a 7, the round is won. If the 7 is rolled before the point, the round is
+ lost. Tally the win or loss.
+*/
+
+public class Craps 
+{
+    
+}
